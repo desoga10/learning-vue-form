55b727a0f597a607ab159f8a3ee29949724c8e7b
desoga10 <done2stay@gmail.com>
2023-06-23 02:18:00 +0100

    Integrated Keyboard events

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -10,10 +10,23 @@
   <option value="developer">Web Developer</option>
   <option value="designer">Web Designer</option>
 </select>
+
+<label>Skills</label>
+<input v-model="tempSkill" type="text" @keyup.alt="addSkill">
+<div v-for="skill in skills" class="pill">
+  {{ skill}}
+</div>
+
+<div class="terms">
+  <input v-model="terms" type="checkbox" required>
+  <label >Accept Terms and Conditions</label>
+</div>
+
   </form>
   <p>Email: {{email}}</p>
   <p>Password: {{password}}</p>
   <p>Role: {{role}}</p>
+  <p>Terms accepted: {{terms}}</p>
 </template>
 <script>
 export default {
@@ -22,7 +35,21 @@ export default {
     return {
       email: '',
       password: '',
-      role: ''
+      role: '',
+      terms: false,
+      tempSkill: '',
+      skills:[]
+    }
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+
+          this.skills.push(this.tempSkill)
+        }
+      this.tempSkill = ''
+     }
     }
   }
 }
@@ -47,7 +74,8 @@ label {
   font-weight: bold;
 }
 
-input, select{
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
@@ -55,5 +83,13 @@ input, select{
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
